caeaae9487886d913bbc751760d7a728ee0d48e2
MilWaxQ <40753865+MilWaxQ@users.noreply.github.com>
2025-01-10 13:53:59 +0100

    Update edzoterem.sql

--- edzoterem.sql
+++ edzoterem.sql
@@ -23,18 +23,39 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
+CREATE TABLE `felhasznalok` (
+  `ID` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `nev` text NOT NULL,
+  `email` text NOT NULL,
+  `jelszo` text NOT NULL,
+  `telefonszam` text NOT NULL,
+  `lakcim` text NOT NULL,
+  `szuletesiDatum` date NOT NULL DEFAULT '2024-12-03',
+  `admin` tinyint(1) NOT NULL,
+  `regisztracioDatuma` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+INSERT INTO `felhasznalok` (`ID`, `nev`, `email`, `jelszo`, `telefonszam`, `lakcim`, `szuletesiDatum`, `admin`, `regisztracioDatuma`) VALUES
+(1, 'Kis Pista', 'kis.pista@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-22-567-8901', 'Budapest, kisbaba u. 11.', '2024-12-03', 1, '2024-11-29 13:05:04'),
+(2, 'Bangó Margit', 'bangomargitka@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-33-456-7890', 'Budapest, markoló utca 123.', '2024-12-03', 0, '2024-11-29 13:06:57'),
+(3, 'Tipp Elek', 'tipp.elek@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-77-314-5433', 'Gyál, makaróni utca 546.', '2024-12-03', 0, '2024-11-29 13:07:34'),
+(4, 'Kovács Viktor', 'viktorkovacs@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-34-432-1235', 'Budapest, billentyűzet utca 365.', '2024-12-03', 0, '2024-11-29 13:08:27'),
+(5, 'Szabó Gizella', 'gizella.szabo@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-98-765-9873', 'Vecsés, brokkoli utca 122.', '2024-12-03', 0, '2024-11-29 13:10:03');
+
 --
 -- Table structure for table `berletek`
 --
 
 CREATE TABLE `berletek` (
-  `ID` varchar(255) NOT NULL,
+  `ID` varchar(255) PRIMARY KEY NOT NULL,
   `lejaratDatuma` date NOT NULL,
   `alkalom` int(11) NOT NULL,
   `berlet` text NOT NULL,
   `lancreakcio` tinyint(1) NOT NULL DEFAULT 0,
   `felhasznaloID` int(11) NOT NULL,
-  `vasarlasDatuma` date NOT NULL DEFAULT '2024-12-03'
+  `vasarlasDatuma` date NOT NULL DEFAULT '2024-12-03',
+  INDEX(felhasznaloID),
+  FOREIGN KEY (`felhasznaloID`) REFERENCES `felhasznalok` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
@@ -52,54 +73,20 @@ INSERT INTO `berletek` (`ID`, `lejaratDatuma`, `alkalom`, `berlet`, `lancreakcio
 -- Table structure for table `felhasznalok`
 --
 
-CREATE TABLE `felhasznalok` (
-  `ID` int(11) NOT NULL,
-  `nev` text NOT NULL,
-  `email` text NOT NULL,
-  `jelszo` text NOT NULL,
-  `telefonszam` text NOT NULL,
-  `lakcim` text NOT NULL,
-  `szuletesiDatum` date NOT NULL DEFAULT '2024-12-03',
-  `admin` tinyint(1) NOT NULL,
-  `regisztracioDatuma` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
 --
 -- Dumping data for table `felhasznalok`
 --
 
-INSERT INTO `felhasznalok` (`ID`, `nev`, `email`, `jelszo`, `telefonszam`, `lakcim`, `szuletesiDatum`, `admin`, `regisztracioDatuma`) VALUES
-(1, 'Kis Pista', 'kis.pista@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-22-567-8901', 'Budapest, kisbaba u. 11.', '2024-12-03', 1, '2024-11-29 13:05:04'),
-(2, 'Bangó Margit', 'bangomargitka@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-33-456-7890', 'Budapest, markoló utca 123.', '2024-12-03', 0, '2024-11-29 13:06:57'),
-(3, 'Tipp Elek', 'tipp.elek@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-77-314-5433', 'Gyál, makaróni utca 546.', '2024-12-03', 0, '2024-11-29 13:07:34'),
-(4, 'Kovács Viktor', 'viktorkovacs@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-34-432-1235', 'Budapest, billentyűzet utca 365.', '2024-12-03', 0, '2024-11-29 13:08:27'),
-(5, 'Szabó Gizella', 'gizella.szabo@gmail.com', '$2b$10$8XzKzcf5yMjF.fl9LXxIuedhJu87o3dqVsVNseux.ZeMRGbvRIOku', '06-98-765-9873', 'Vecsés, brokkoli utca 122.', '2024-12-03', 0, '2024-11-29 13:10:03');
+
 
 --
 -- Indexes for dumped tables
 --
 
---
--- Indexes for table `berletek`
---
-ALTER TABLE `berletek`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `felhasznalok`
---
-ALTER TABLE `felhasznalok`
-  ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `felhasznalok`
---
-ALTER TABLE `felhasznalok`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -108,9 +95,6 @@ ALTER TABLE `felhasznalok`
 --
 -- Constraints for table `felhasznalok`
 --
-ALTER TABLE `felhasznalok`
-  ADD CONSTRAINT `felhasznalok_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `berletek` (`felhasznaloID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
